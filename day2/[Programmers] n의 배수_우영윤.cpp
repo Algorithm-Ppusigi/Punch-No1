402352ccd7d61c1b938717f3f7dbd74afddf4f32
Yeongyun Woo <woo036325@gmail.com>
2024-06-27 17:29:00 +0900

    solved: 프로그래머스 n의 배수

---
+++ day2/[Programmers] n의 배수_우영윤.cpp
@@ -0,0 +1,12 @@
+/*
+	Author: Yeongyun Woo
+	Date: 2024-06-26
+	Description:
+		- n의 배수 여부를 판별하는 문제
+	Link: https://school.programmers.co.kr/learn/courses/30/lessons/181937
+*/
+
+int solution(int num, int n)
+{
+	return !(num % n);
+}
